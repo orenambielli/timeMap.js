0178875edc7e89a237791383e2debd1649f72b91
Oren Ambielli <orenambielli@gmail.com>
2018-11-02 11:49:35 -0400

    Update jScript.js

---
+++ jScript.js
@@ -0,0 +1,28 @@
+// js file is loaded after jQuery //
+
+// 'Where Time Is It?' is a small js app I built
+// to convert the current date and time 
+// into map coordinates that will display a
+// Google map result into an iFrame.
+
+var currentTime = new Date();
+var h = currentTime.getHours();
+var mm = currentTime.getMinutes();
+var s = currentTime.getSeconds();
+var latitude = (h + "%20" + mm + "'" + s);
+var displayLat = (h + "&deg;" + mm + " " + s)
+
+var m = currentTime.getMonth()+1;
+var d = currentTime.getDate();
+var y = currentTime.getFullYear() - 2000;
+var longitude = m + "%20" + d + "'" + y;
+var displayLong = (m + "&deg;" + d + " " +  y)
+//murican calendar system!
+
+document.write("<p class = 'coordinates'>Your longitude is month/day/year, or " + displayLong + ".</p>");
+document.write("<p class = 'coordinates'>Your latitude is hours/minutes/seconds, or " + displayLat + ".</p>");
+
+document.write('<div class="map"><div class=  "gmap_canvas"><iframe width = "600" height = "500" id = "gmap_canvas" src = "https://maps.google.com/maps?q=' + longitude + '%20' + latitude + '&t=k&z=13&ie=UTF8&iwloc=&output=embed" frameborder= "0" scrolling= "no" marginheight= "0" marginwidth= "0"></iframe></div></div>')
+
+
+console.log(longitude + " " + latitude);
